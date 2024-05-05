43d092442b4a86d47a26a3c8b1423d1e56f7ec2e
Seth Herr <seth.william.herr@gmail.com>
2024-05-05 13:26:11 -0700

    Competition activities

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,8 @@
 class LandingController < ApplicationController
   def index
     @competition = Competition.current || Competition.create(start_date: Date.parse("2024-5-1"))
-    @competition_users = @competition.competition_users_included.includes(:user).score_ordered
+    @competition_users = @competition.competition_users_included
+      .includes(:user, :competition_activities).score_ordered
     @page_title = "May is Bike Month"
   end
 
