8630bac414f6a7a6d907db22652d237cb21aa409
Seth Herr <seth.william.herr@gmail.com>
2025-04-15 12:13:47 -0700

    add .tool-versions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.cache_store = :redis_cache_store, {url: config.redis_cache_url}
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter = :resque
+  config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "may_is_bike_month_production"
 
   config.action_mailer.perform_caching = false
