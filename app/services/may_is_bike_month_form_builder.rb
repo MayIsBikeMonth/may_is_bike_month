f2eba061c8db31869b443da1323bc44d9d42a1d9
Seth Herr <seth.william.herr@gmail.com>
2026-04-01 10:40:40 -0400

    rename form builder

--- app/services/may_is_bike_month_form_builder.rb
+++ app/services/may_is_bike_month_form_builder.rb
@@ -1,3 +1,3 @@
 # ViewComponents require a custom form builder, the default FormBuilder modifies global state
-class SethTemplateFormBuilder < ActionView::Helpers::FormBuilder
+class MayIsBikeMonthFormBuilder < ActionView::Helpers::FormBuilder
 end
