f4f5b2eaa5cb4a3adffe922e8f5c3dce3a4feccd
Seth Herr <seth.william.herr@gmail.com>
2026-04-16 16:56:11 -0700

    Add competitions original static rendering (#76)
    Also a `OtherCompetitionLinks` component

---
+++ app/controllers/competitions_original_controller.rb
@@ -0,0 +1,11 @@
+class CompetitionsOriginalController < ApplicationController
+  AVAILABLE_YEARS = %w[2024 2025].freeze
+
+  def show
+    @year = params[:id]
+    raise ActionController::RoutingError, "Not Found" unless AVAILABLE_YEARS.include?(@year)
+    @competitions = AVAILABLE_YEARS.map { |y| Competition.new(start_date: Date.new(y.to_i, 5, 1)) }
+    @page_title = "May is Bike Month #{@year}"
+    @skip_wrapper_class = true
+  end
+end
