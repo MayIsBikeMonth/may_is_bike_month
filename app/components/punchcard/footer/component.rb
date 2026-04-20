829c3eca7b42d19feae289dc5e45e62cf7119af9
Seth Herr <seth.william.herr@gmail.com>
2026-04-20 12:46:24 -0700

    forever

--- app/components/punchcard/footer/component.rb
+++ app/components/punchcard/footer/component.rb
@@ -4,14 +4,6 @@ module Punchcard::Footer
   class Component < ApplicationComponent
     MILE_KM = 1.609344
 
-    LEVEL_COLOR_CLASSES = {
-      1 => "bg-purple-200 dark:bg-purple-900",
-      2 => "bg-purple-300 dark:bg-purple-800",
-      3 => "bg-purple-400 dark:bg-purple-700",
-      4 => "bg-purple-500 dark:bg-purple-600",
-      5 => "bg-purple-600 dark:bg-purple-500"
-    }.freeze
-
     def initialize(updated_at:, competition:, competitions: [])
       @updated_at = updated_at
       @competition = competition
@@ -25,7 +17,7 @@ module Punchcard::Footer
       pairs.map.with_index do |(level, min_mi), i|
         next_mi = pairs[i + 1]&.last
         {
-          color_class: LEVEL_COLOR_CLASSES[level],
+          level:,
           title_imperial: range_title(min_mi, next_mi, "mi"),
           title_metric: range_title(min_mi * MILE_KM, next_mi&.*(MILE_KM), "km")
         }
