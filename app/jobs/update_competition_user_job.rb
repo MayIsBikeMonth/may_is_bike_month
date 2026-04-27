1e1b8571dab1b1cf2cd7f57433ba69496e0b259b
Seth Herr <seth.william.herr@gmail.com>
2026-04-27 08:36:05 -0700

    Stop erroring for competition users not in competition (#119)

--- app/jobs/update_competition_user_job.rb
+++ app/jobs/update_competition_user_job.rb
@@ -19,12 +19,9 @@ class UpdateCompetitionUserJob < ApplicationJob
     return self.class.enqueue_current if id.nil?
 
     competition_user = CompetitionUser.find(id)
-    if competition_user.excluded_from_competition?
-      # Added this constraint, because if "included_in_competition" changes,
-      # it doesn't correctly re-enqueue updating
-      raise "Competition user isn't included in competition!"
-    end
-    return unless User.valid_strava_auth?(competition_user.user.strava_auth)
+    return unless competition_user.included_in_competition? &&
+      User.valid_strava_auth?(competition_user.user.strava_auth)
+
     StravaRequest.update_competition_user_activities(competition_user)
     competition_user.reload.update_score_data!
     Leaderboard::PunchcardWrapper::Component.broadcast_refresh_current!
